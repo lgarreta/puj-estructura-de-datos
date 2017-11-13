0ee923284a7e529cca9cb45f0c2b1374620479e0
Luis Garreta <lgarreta@gmail.com>
2017-11-13 12:38:11 -0500

    bn2

---
+++ clases/Parcial03-ListasNodos-Hash-Archivos/proyecto-BatallaNaval/lnx/mitablero.cpp
@@ -0,0 +1,14 @@
+#include "mitablero.h"
+
+// Representa el tablero donde se pintan los barcos y los disparos
+
+// Retorn el numero de filas
+int MiTablero::getFilas () { 
+		return nFilas;
+}
+
+// Retorn el numero de columnas
+int MiTablero::getColumnas () { 
+		return nColumnas;
+}
+
