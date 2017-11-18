17a49673303395eadaba295abdab154d617ae567
Luis Garreta <lgarreta@gmail.com>
2017-11-18 11:42:43 -0500

    listanodos

---
+++ taller/listas-nodos/lnx/listanodos.h
@@ -0,0 +1,31 @@
+#include<iostream>
+
+using namespace std;
+struct nodo {
+    int data;
+    nodo *next;
+};
+
+class lista {
+private:
+    nodo *head, *tail;
+
+		nodo* crearnodo(int valor){
+			nodo *n = new nodo;
+			n-> data = valor;
+			n-> next = NULL;
+		}
+
+public:
+  lista();
+	void adicionar(int valor);
+	void insertar(int pos, int valor);
+	void mostrarse();
+	void eliminar(int pos);
+	int getElemento (int x);
+	void setElemento (int valor, int x);
+	int longitud ();
+	bool estaVacia ();
+	int buscarElemento (int valor);
+	bool existeElemento (int valor);
+};
---
+++ taller/listas-nodos/win/mainlistas.cpp
@@ -0,0 +1,14 @@
+#include "listanodos.h"
+
+int main(){
+	lista l;
+
+	l.adicionar (10);
+	l.adicionar (20);
+	l.adicionar (30);
+	l.adicionar (40);
+
+	l.mostrarse();
+
+	
+}
